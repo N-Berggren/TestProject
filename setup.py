0b2dba5378edb3238bba27e500b6d340d97d4c7d
N-Berggren <nils.berggren@osterskar.se>
2021-03-11 12:16:12 +0100

    1st commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='TestProject',
+    version='1.0.0',
+    packages=['src'],
+    url='',
+    license='',
+    author='Nils Berggren',
+    author_email='nils.berggren@myconit.se',
+    description='just a test project'
+)
---
+++ src/hello_world.py
@@ -0,0 +1,13 @@
+class HelloWorld:
+    def __init__(self):
+        print("Hello World!")
+
+
+def main():
+    HelloWorld()
+
+
+# ========================================
+# main starts here
+if __name__ == "__main__":
+    main()
